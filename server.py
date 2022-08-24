4361ace41ab4a9257dd89424696dda1d7a1daece
Robert Teets <teetsrobert@gmail.com>
2022-08-24 14:29:39 -0600

    assignment requirements satisfied

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 app.secret_key = 'secrets are no fun'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 1
+
+    return render_template('index.html', counter=session['counter'])
+
+@app.route('/destroy_session', methods=['POST'])
+def destroy_session():
+    session.clear()
+    return redirect('/')
     
 if __name__ == '__main__':
     app.run(debug=True)
